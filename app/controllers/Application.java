62b2b7039a7ece89256d2c361120a721fd98fbfa
edwardsbean <edwardsbean@gmail.com>
2015-02-12 18:42:27 +0800

    add file upload

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,15 +1,24 @@
 package controllers;
 
 import models.User;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
 import play.*;
 import play.data.Form;
 import play.libs.Json;
 import play.mvc.*;
+import registry.machine.RegistryMachineContext;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Application extends Controller {
     public static Logger.ALogger log = Logger.of("application");
-
+    public static RegistryMachineContext registryMachineContext = new RegistryMachineContext();
+    
     public static Result index() {
         return ok("ok");
     }
@@ -27,4 +36,42 @@ public class Application extends Controller {
         return ok(Json.toJson(user));
     }
 
+    public static Result upload(String name) {
+        Http.MultipartFormData body = request().body().asMultipartFormData();
+        if (body.getFiles().isEmpty()) {
+            return badRequest("fail");
+        } else {
+            File file = body.getFile("file").getFile();
+            LineIterator iterator;
+            try {
+                iterator = FileUtils.lineIterator(file);
+            } catch (IOException e) {
+                return badRequest("文件错误");
+            }
+            List<String> lists = new ArrayList<>();
+            while (iterator.hasNext()) {
+                String line = iterator.nextLine();
+                log.debug("{}内容：{}", name, line);
+                lists.add(line);
+            }
+            if ("proxy".equals(name)) {
+                registryMachineContext.addProxies(lists);
+            } else if ("email".equals(name)) {
+                registryMachineContext.addEmails(lists);
+            } else {
+                return badRequest("文件类型未知");
+            }
+            return ok("ok");
+        }
+//        if (file != null) {
+//            String fileName = file.getFilename();
+//            String contentType = file.getContentType();
+//            File saved = file.getFile();
+//            System.out.println(fileName);
+//            return ok("File uploaded");
+//        } else {
+//            flash("error", "Missing file");
+//            return redirect(routes.Application.index());
+//        }
+    }
 }
