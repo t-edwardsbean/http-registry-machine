2730c25d0e9f3cecab63d340c13d4060c66d9e24
edwardsbean <edwardsbean@gmail.com>
2015-02-16 19:03:52 +0800

    爱码添加加黑号码

--- app/registry/machine/AIMA.java
+++ app/registry/machine/AIMA.java
@@ -12,7 +12,6 @@ import java.util.regex.Pattern;
  */
 public class AIMA {
     private static Logger log = LoggerFactory.getLogger(AIMA.class);
-
     private String uid;
     private String pwd;
     private String pid;
@@ -20,6 +19,7 @@ public class AIMA {
     private String AIMA_LOGIN_URL = "http://api.f02.cn/http.do?action=loginIn&uid=%s&pwd=%s";
     private String AIMA_GET_PHONE_URL = "http://api.f02.cn/http.do?action=getMobilenum&pid=%s&uid=%s&mobile=&size=1&token=";
     private String AIMA_GET_CODE_URL = "http://api.f02.cn/http.do?action=getVcodeAndReleaseMobile&uid=%s&token=%s&mobile=";
+    private String AIMA_IGNORE = "http://api.f02.cn/http.do?action=addIgnoreList&uid=%s&token=%s&pid=%s&mobiles=";
 
 
     public AIMA(String uid, String pwd, String pid) {
@@ -40,6 +40,7 @@ public class AIMA {
             log.debug("成功登录爱玛平台，Response:" + loginResult);
             RegistryMachineContext.logger.tell("成功登录爱玛平台，Response:" + loginResult, ActorRef.noSender());
             log.debug("成功登录爱玛平台，Token:" + token);
+            AIMA_IGNORE = String.format(AIMA_IGNORE, uid, token, pid);
             RegistryMachineContext.logger.tell("成功登录爱玛平台，Token:" + token, ActorRef.noSender());
             AIMA_GET_PHONE_URL = String.format(AIMA_GET_PHONE_URL, pid, uid) + token;
             AIMA_GET_CODE_URL = String.format(AIMA_GET_CODE_URL, uid, token);
@@ -48,17 +49,25 @@ public class AIMA {
         }
     }
 
+    public void addIgnoreList(Task task) {
+        log.debug("爱码平台未返回或返回错误验证码，加黑无用手机号码");
+        LogUtils.log(task, "爱码平台未返回或返回错误验证码，加黑无用手机号码");
+        String result = HttpUtils.Get(AIMA_IGNORE + task.getEmail());
+        log.debug("加黑结果：" + result);
+        LogUtils.log(task, "加黑结果：" + result);
+    }
+
     public String getPhone(Task task) {
         log.debug("请求手机号:" + AIMA_GET_PHONE_URL);
         LogUtils.log(task, "请求手机号:" + AIMA_GET_PHONE_URL);
         String getPhoneResult = HttpUtils.Get(AIMA_GET_PHONE_URL);
         int phoneLength = getPhoneResult.split("\\|").length;
-        log.debug("检测爱玛平台获取的手机号码：" + getPhoneResult);
-        LogUtils.log(task, "检测爱玛平台获取的手机号码：" + getPhoneResult);
+        log.debug("检测爱码平台获取的手机号码：" + getPhoneResult);
+        LogUtils.log(task, "检测爱码平台获取的手机号码：" + getPhoneResult);
         if (phoneLength == 2) {
             return getPhoneResult.split("\\|")[0];
         } else {
-            throw new MachineException("请求爱玛返回手机号码，无法识别返回数据:" + getPhoneResult);
+            throw new MachineException("请求爱码返回手机号码，无法识别返回数据:" + getPhoneResult);
         }
     }
 
@@ -89,6 +98,7 @@ public class AIMA {
                 LogUtils.log(task, "验证码不对，继续等待");
             }
         }
+        addIgnoreList(task);
         throw new MachineException(LogUtils.format(task, "向爱玛平台索要验证码超时"));
     }
 }
